38001da1d06dc112db6bc4d83f70a9b043efd704
ZockerKatze <basti.zinkl10@gmail.com>
2025-03-15 20:10:19 +0100

    some bullshit

--- msw/mainblog/qoutes/words/script.js
+++ msw/mainblog/qoutes/words/script.js
@@ -1,11 +1,10 @@
 /************************************************************
-* This Code is a Pain in the ass to read (you like it tho)  *
+*                  This Code is copied xD                   *
 *************************************************************/
 
 document.getElementById("darkModeToggle").addEventListener("click", function() {
     document.body.classList.toggle("dark-mode");
 });
-
 document.querySelectorAll('.changefile-button').forEach(button => {
     button.style.textDecoration = 'none';
     button.style.color = 'black';
